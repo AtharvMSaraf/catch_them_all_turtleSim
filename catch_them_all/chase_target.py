95e871556ea485402b4d2050f47858e540d58e81
AtharvMSaraf <atharvmakarandsaraf@gmail.com>
2025-04-08 21:35:32 -0700

    1st chase success.... respaw has error

--- catch_them_all/chase_target.py
+++ catch_them_all/chase_target.py
@@ -7,6 +7,7 @@ from my_robot_interfaces.msg import TargetPositionCordinates
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 import math
+from my_robot_interfaces.srv import KillTurtle
 
 
 class chaseNode(Node):
@@ -22,30 +23,60 @@ class chaseNode(Node):
         # subscribing to main turtle position
         self.turtle_pose_sub = self.create_subscription(Pose,"/turtle1/pose",self.callback_pose_sub,10)
 
+        #initiating kill client:
+        self.kill_client = self.create_client(KillTurtle,"kill_catched_turtle")
+
     def callback_target_sub(self,data):
         self.target_pose_x = data.target_pose[0]
         self.target_pose_y = data.target_pose[1]
-        self.get_logger().info("target = " + str(self.target_pose_x) + str(self.target_pose_y))
+        # self.get_logger().info("target = " + str(self.target_pose_x) + str(self.target_pose_y))
 
     def callback_pose_sub(self,pose):
         # ***
         # function decides whether to kill a spawn or not
         # ***
 
+
         cmd_msg = Twist()
         if not (self.target_pose_x == None) or (self.target_pose_y == None):
-            self.get_logger().info("rotating")
-            if pose.x != self.target_pose_x:
-                cmd_msg.linear.x = 1.0
-            else:
-                cmd_msg.linear.x = 1.0
+            
+            slope = (self.target_pose_y - pose.y) / (self.target_pose_x - pose.x)
+            angle_radians = math.atan2((self.target_pose_y - pose.y),(self.target_pose_x - pose.x))
 
-            if pose.y != self.target_pose_y:
-                cmd_msg.linear.y = 1.0
-            else:
-                cmd_msg.linear.y = 0.0
+            # Allowable error for angle comparison
+            epsilon = 0.09
+            turn_flag = True
 
-        self.cmd_vel_pub.publish(cmd_msg)
+            # Compare the angles with a tolerance for precision issues
+            if (abs(pose.theta - angle_radians) > epsilon):
+                self.get_logger().info("rotating") 
+                cmd_msg.angular.z = 1.0  # Rotate if not aligned
+            else:
+                cmd_msg.angular.z = 0.0  # Stop rotation once aligned
+
+            if (((abs(pose.x - self.target_pose_x) > (epsilon)) or (abs(pose.y - self.target_pose_y) > (epsilon) )) ):
+                self.get_logger().info("moving") 
+                cmd_msg.linear.x = 0.5  
+            else:
+                cmd_msg.linear.x = 0.0 
+                self.chase_success_client_call()
+
+            # Publish the movement commanda
+            self.cmd_vel_pub.publish(cmd_msg)
+    
+    def chase_success_client_call(self):
+
+        while not self.kill_client.wait_for_service():
+            self.get_logger().warning("Waiting for the add two int service ...")
+        request = KillTurtle.Request()
+        request.confirm_kill = True
+        future = self.kill_client.call_async(request)
+        future.add_done_callback(self.callback_respawn_service_done)
+
+    def callback_respawn_service_done(self,future):
+        response = future.result()
+        # self.get_logger().info(str(response))
+
         
 def main(args=None):
     rclpy.init(args=args)
--- catch_them_all/turtle_respawn_service.py
+++ catch_them_all/turtle_respawn_service.py
@@ -5,6 +5,7 @@ from rclpy.node import Node
 from turtlesim.srv import Spawn, Kill
 from turtlesim.msg import Pose
 from my_robot_interfaces.msg import TargetPositionCordinates
+from my_robot_interfaces.srv import KillTurtle
 
 import random
 
@@ -30,8 +31,8 @@ class respawnNode(Node):
         #clock for publishing the msg every 0.1 sec 
         self.clk = self.create_timer(0.011,self.publish_target)
 
-        # subscribing to main turtle position
-        self.sub = self.create_subscription(Pose,"/turtle1/pose",self.callback_pose_sub,10)
+        #kill top turtle srv:
+        self.kill_srv = self.create_service(KillTurtle,"kill_catched_turtle",self.callback_kill_catched_turtle)
 
     def spawn_turtle_client_call(self):
         self.random_name = "spawn" + str(random.randint(0,100))
@@ -39,8 +40,8 @@ class respawnNode(Node):
             self.get_logger().warning("Waiting for the add two int service ...")
 
         request = Spawn.Request() 
-        x = random.uniform(0,10)
-        y = random.uniform(0,10)
+        x = random.uniform(0,9)
+        y = random.uniform(0,9)
         request.x = x
         request.y = y
         request.name = self.random_name
@@ -59,29 +60,9 @@ class respawnNode(Node):
         msg = TargetPositionCordinates()
         msg.target_pose = list(self.spawn_pose_dic.values())[0]
         self.target_publisher.publish(msg)
-
-    def callback_pose_sub(self,pose):
-        # ***
-        # function decides whether to kill a spawn or not
-        # ***
-
-        main_turtle_pose_array = [int(pose.x), int(pose.y)]
-        # self.get_logger().info(str(main_turtle_pose_array))
-
-        if main_turtle_pose_array in self.spawn_pose_dic.values():
-            self.get_logger().info("killed")
-            for k,v in self.spawn_pose_dic.items():
-                kill_turtle_key = None
-                if v == main_turtle_pose_array:
-                    kill_turtle_key = k
-                    break
-            del self.spawn_pose_dic[kill_turtle_key]
-            self.kill_spwan_client_call(kill_turtle_key)
-            self.spawn_turtle_client_call()
             
     
     def kill_spwan_client_call(self,name):
-
 
         while not self.kill_client.wait_for_service():
             self.get_logger().warning("Waiting for the add two int service ...")
@@ -91,6 +72,13 @@ class respawnNode(Node):
         self.get_logger().info("Killed " + str(request.name))
         future = self.kill_client.call_async(request)
         future.add_done_callback(self.callback_respawn_service_done)
+
+    def callback_kill_catched_turtle(self, request: KillTurtle.Request, response: KillTurtle.Response ):
+        list(self.spawn_pose_dic.values()).pop(0)
+        spawn_name = list(self.spawn_pose_dic.keys())[0]
+        self.kill_spwan_client_call(spawn_name)
+        self.spawn_turtle_client_call()
+
 
 
 def main(args=None):
